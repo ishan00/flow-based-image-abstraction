aed79d4ea1488a71274a7ce4d580d0c6b67647a7
Ishan Tarunesh <ishantarunesh@gmail.com>
2018-11-21 00:39:21 +0530

    Working

--- see.m
+++ see.m
@@ -1,14 +1,22 @@
-img = load('data.mat');
+img = load('macaw.mat');
 H1 = img.H1;
 H2 = img.H2;
-r = 1.5;
-%{
-subplot(1,2,1)
-imshow(H1,[]);
-subplot(1,2,2)
-imshow(H2,[]);
-%}
+tang = img.tang;
+r = 0.999;
 
+
+figure(1);
+subplot(2,2,1)
+imshow(tang(:,:,1),[]);
+subplot(2,2,2)
+imshow(tang(:,:,2),[]);
+subplot(2,2,3)
+imshow(H1,[]);
+subplot(2,2,4)
+imshow(H2,[]);
+
+figure(2);
 ind1 = H2 < 0;
-ind2 = (1 + tanh(H2) < r);
+ind2 = ((1 + tanh(H2)) < r);
+
 imshow(ind1.*ind2,[]);
